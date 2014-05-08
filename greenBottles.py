f7a1657f74acd2886434d76a4f48c437164353da
SWhale6988 <Swhale6988@soham-college.org.uk>
2014-05-08 11:44:36 +0100

    Update greenBottles.py

--- greenBottles.py
+++ greenBottles.py
@@ -1,4 +1,10 @@
-print("10 green bottles, hanging on the wall")
-print("10 green bottles, hanging on the wall")
-print("And if 1 green bottle should acidentally fall,")
-print("They'll be 9 green bottles hanging on the wall.")
+from time import sleep
+count = 10
+for x in range(10):
+    for y in range(2):
+        print(str(count) + " green bottles, hanging on the wall")
+        
+    print("And if 1 green bottle should acidentally fall,")
+    print("They'll be " + str(count-1) + " green bottles hanging on the wall.\n")
+    sleep(1)
+    count -= 1
